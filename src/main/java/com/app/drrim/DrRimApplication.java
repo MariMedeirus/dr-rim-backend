0f33b91eac8ce827e211baaa4918ae5d1d16f1bf
marimedeirus <mariana_medeiros.santos@outlook.com>
2025-05-20 15:10:36 -0300

    first commit

---
+++ src/main/java/com/app/drrim/DrRimApplication.java
@@ -0,0 +1,13 @@
+package com.app.drrim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrRimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrRimApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/drrim/DrRimApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.drrim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrRimApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

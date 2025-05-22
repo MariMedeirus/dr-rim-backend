62768cd5fd72bd17c1bb998272a606db4af33d01
marimedeirus <mariana_medeiros.santos@outlook.com>
2025-05-22 02:04:15 -0300

    Creating Post entity with nested User

--- src/main/java/com/app/drrim/domain/Post.java
+++ src/main/java/com/app/drrim/domain/Post.java
@@ -1,23 +1,31 @@
 package com.app.drrim.domain;
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.io.Serializable;
+import java.util.Date;
 
 @Data
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @NoArgsConstructor
 @AllArgsConstructor
+
 @Document
-public class User implements Serializable {
+public class Post implements Serializable {
     private static final long serialVersionUID = 1L;
 
     @Id
     @EqualsAndHashCode.Include
     private String id;
-    private String name;
-    private String email;
+
+    private Date date;
+    private String title;
+    private String body;
+    private User author;
 
 }
---
+++ src/main/java/com/app/drrim/repository/PostRepository.java
@@ -0,0 +1,10 @@
+package com.app.drrim.repository;
+
+import com.app.drrim.domain.Post;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String> {
+
+}
